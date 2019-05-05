fe9668a174e3a1fa22840c3266c84df1957abff4
Saiful <icungse@gmail.com>
2019-05-05 21:24:49 +0700

    first commit

---
+++ BullEye/AboutAuthorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutAuthorViewController.swift
+//  BullEye
+//
+//  Created by icungse on 16/03/19.
+//  Copyright © 2019 icung. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
